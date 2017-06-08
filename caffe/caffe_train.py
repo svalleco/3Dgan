3232e280b932094723a9b02625ee9c4bfc252059
Gul Rukh Khattak <gkhattak@rudra.dyndns.cern.ch>
2017-06-08 11:29:51 +0200

    Updated training file

--- caffe/caffe_train.py
+++ caffe/caffe_train.py
@@ -30,9 +30,11 @@ max_iter = 1161  # maximum number of iterations
 display_every = 100 # show losses every so many iterations
 snapshot_every = 500 # snapshot every so many iterations
 snapshot_folder = 'snapshots_test' # where to save the snapshots (and load from)
+snapshot_at_iter = -1
+snapshot_at_iter_file = 'snapshot_at_iter.txt'
 gpu_id = 0
 num_clas = 2
-
+lr = 0.9
 sub_nets = ('generator2', 'discriminator2', 'data2')
 
 #initialize the nets
@@ -59,17 +61,18 @@ if start_snapshot:
 
 
 #read weights of losses                                                         
-discr_loss_weight = discriminator.net._blob_loss_weights   #  he used[discriminator.net._blob_names_index['discr_loss']]
+#discr_loss_weight = discriminator.net._blob_loss_weights   #  he used[discriminator.net._blob_names_index['discr_loss']]
 
 #do training                                                                    
 start = time.time()
 #do training                                                                    
 start = time.time()
 for it in range(start_snapshot,max_iter):
-  # read the data                                                               
+
+  # read the data
   data_reader.net.forward()
 
-  # feed the data to the generator and run it 
+  # feed the data to the generator and Get the generated image 
   noise = np.random.normal(0, 1, (batch_size, latent)).astype(np.float32)
   sampled_labels = np.random.randint(0, num_clas, batch_size)
   generator.net.blobs['feat'].data[...] = noise
@@ -77,45 +80,51 @@ for it in range(start_snapshot,max_iter):
   generator.net.forward()
   generated_img = generator.net.blobs['generated'].data
 
-  # run the discriminator on real data                                          
-  discriminator.net.blobs['ECAL'].data[...] = data_reader.net.blobs['data'].data
+  # run the discriminator on real data                                         
+  discriminator.net.blobs['ECAL'].data[...] = data_reader.net.blobs['ECAL'].data
   discriminator.net.blobs['TAG'].data[...] = data_reader.net.blobs['TAG'].data
   discriminator.net.blobs['event'].data[...] = bit_flip(np.ones((batch_size,1), dtype='float32'))          
   discriminator.net.forward()
-  discr_real_loss = np.copy(discriminator.net.blobs['discr_loss'].data)
-  discriminator.increment_iter()
-  discriminator.net.clear_param_diffs()
-  discriminator.net.backward()
+  discr_real_loss = np.copy(discriminator.net.blobs['loss'].data)
+ # discriminator.increment_iter()
+  # discriminator.net.clear_param_diffs()
+  # discriminator.net.backward()
+  discriminator.step(1)             # train the discriminator
 
-  # run the discriminator on generated data                                     
+  # run the discriminator on generated data 
   discriminator.net.blobs['ECAL'].data[...] = generated_img
-  discriminator.net.blobs['TAG'].data[...] = sampled_labels.reshape((-1, 1))
+  discriminator.net.blobs['TAG'].data[...] = sampled_labels #.reshape((-1, 1))
   discriminator.net.blobs['event'].data[...] = bit_flip(np.zeros((batch_size,1), dtype='float32'))
   discriminator.net.forward()
-  discr_fake_loss = np.copy(discriminator.net.blobs['discr_loss'].data)
-  discriminator.net.backward()
-  discriminator.apply_update()
+  discr_fake_loss = np.copy(discriminator.net.blobs['loss'].data)
+  #discriminator.net.backward()
+  discriminator.step(1)
+  #discriminator.apply_update()
 
-# run the discriminator on generated data with opposite labels, to get the gradient for the generator                                                        
-  discriminator.net.blobs['event'].data[...] =bit_flip( np.ones((batch_size,1), dtype='float32'))
-  discriminator.net.forward_simple()
-  discr_fake_for_generator_loss = np.copy(discriminator.net.blobs['discr_loss'].data)
-  generator.increment_iter()
-  generator.net.clear_param_diffs()                                             
-  discriminator.net.backward()
+# run the discriminator on generated data with opposite labels, to get the gradient for the generator
+  for i in range(2):                                                        
+     discriminator.net.blobs['event'].data[...] =bit_flip( np.ones((batch_size,1), dtype='float32'))
+     discriminator.net.forward()
+     discr_fake_for_generator_loss = np.copy(discriminator.net.blobs['loss'].data)
+ # generator.increment_iter()
+     generator.net.clear_param_diffs()    
+     discriminator.net.backward()
                                         
 # Train the generator
-  generator.net.blobs['generated'].diff[...] = discriminator.net.blobs['data'].diff
-  generator.net.backward()
-  generator.apply_update()
-
+     generator.net.blobs['generated'].diff[...] = discriminator.net.blobs['ECAL'].diff
+     generator.net.backward()
+  #generator.apply_update()
+  #generator.update()
+     for layer in generator.net.layers:
+        for blob in layer.blobs:
+            blob.data[...] -= lr * blob.diff
 # add by samson                                                                                                         
 #display                                                                      
   if it % display_every == 0:
     print >> sys.stderr, "[%s] Iteration %d: %f seconds" % (time.strftime("%c"), it, time.time()-start)
-    print >> sys.stderr, "  discr real loss: %e * %e = %f" % (discr_real_loss, discr_loss_weight, discr_real_loss*discr_loss_weight)
-    print >> sys.stderr, "  discr fake loss: %e * %e = %f" % (discr_fake_loss, discr_loss_weight, discr_fake_loss*discr_loss_weight)
-    print >> sys.stderr, "  discr fake loss for generator: %e * %e = %f" % (discr_fake_for_generator_loss, discr_loss_weight, discr_fake_for_generator_loss*discr_loss_weight)
+    print >> sys.stderr, "  discr real loss:  %f" % (discr_real_loss)
+    print >> sys.stderr, "  discr fake loss:  %f" % (discr_fake_loss)
+    print >> sys.stderr, "  discr fake loss for generator: %f" % (discr_fake_for_generator_loss)
     start = time.time()
     if os.path.isfile(snapshot_at_iter_file):
       with open (snapshot_at_iter_file, "r") as myfile:
