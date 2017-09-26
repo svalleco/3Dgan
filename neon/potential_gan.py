ed71548e9407b60be2bed9515ceaadb0683e8d53
Sofia Vallecorsa <svalleco@macbooksofia.dyndns.cern.ch>
2017-09-26 14:35:43 +0200

    Switch vanilla case to hdf5iterator

--- neon/potential_gan.py
+++ neon/potential_gan.py
@@ -11,35 +11,20 @@ from neon.transforms import Rectlin, Logistic, GANCost, Tanh
 from neon.util.argparser import NeonArgparser
 from neon.util.persist import ensure_dirs_exist
 from neon.layers.layer import Dropout
-from neon.data.dataiterator import HDF5Iterator
+from neon.data.hdf5iterator import HDF5Iterator
 from neon.optimizers import GradientDescentMomentum, RMSProp, Adam
 from gen_data_norm import gen_rhs
 from neon.backends import gen_backend
 from temporary_utils import temp_3Ddata
 import numpy as np
-from sklearn.model_selection import train_test_split
 # import matplotlib.pyplot as plt
 import h5py
 
 # load up the data set
-X, y = temp_3Ddata()
-X[X < 1e-6] = 0
-mean = np.mean(X, axis=0, keepdims=True)
-max_elem = np.max(np.abs(X))
-print(np.max(np.abs(X)),'max abs element')
-print(np.min(X),'min element')
-X = (X- mean)/max_elem
-print(X.shape, 'X shape')
-print(np.max(X),'max element after normalisation')
-print(np.min(X),'min element after normalisation')
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.9, random_state=42)
-print(X_train.shape, 'X train shape')
-print(y_train.shape, 'y train shape')
 
-gen_backend(backend='gpu', batch_size=100)
 print 'starting HDF5Iterator'
-train_set = HDF5Iterator(X=X_train, y=y_train, nclass=2, lshape=(1, 25, 25, 25))
-valid_set = HDF5Iterator(X=X_test, y=y_test, nclass=2)
+train_set = HDF5Iterator('EGshuffled_train.h5')
+valid_set = HDF5Iterator('EGshuffled_test.h5')
 
 print 'train_set OK'
 #tate=lt.plot(X_train[0, 12])
