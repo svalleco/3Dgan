4b495b507326186444772739819d98ebd2b28c3c
Gul Rukh Khattak <gkhattak@rudra.dyndns.cern.ch>
2018-03-30 17:39:53 +0200

    Root plots

--- keras/root/root_fit.py
+++ keras/root/root_fit.py
@@ -1,5 +1,5 @@
 from os import path
-from ROOT import TCanvas, TGraph, gStyle, TProfile
+from ROOT import TCanvas, TGraph, gStyle
 #from ROOT import gROOT, gBenchmark
 import h5py
 import numpy as np
@@ -10,41 +10,39 @@ from array import array
 
 gStyle.SetOptFit (111) # superimpose fit results
 c1=TCanvas("c1" ,"Data" ,200 ,10 ,700 ,500) #make nice
-c1.SetGrid()
-c1.SetLogx ()
-Eprof = TProfile("Eprof", "Ecal/Ep versus Ep;Ep GeV; Ecal/Ep", 100, 0, 5)
-num_events=10000
-#d=h5py.File("/eos/project/d/dshep/LCD/V1/EleEscan/EleEscan_1_1.h5")
-d=h5py.File("/afs/cern.ch/work/g/gkhattak/public/Ele_v1_1_2.h5",'r')
-X=np.array(d.get('ECAL')[0:num_events], np.float64)                             
-Y=np.array(d.get('target')[0:num_events][:,1], np.float64)
+c1.SetGrid ()
 
+num_events=10000
+d=h5py.File("/afs/cern.ch/work/g/gkhattak/public/Ele_v1_1_2.h5",'r')
+X=np.array(d.get('ECAL')[0:num_events], np.float32)                             
+Y=np.array(d.get('target')[0:num_events][:,1], np.float32)
 # Initialization of parameters
 X[X < 1e-6] = 0
-Y = Y/100
+Y = Y
 Data = np.sum(X, axis=(1, 2, 3))
 a = np.ndarray( [num_events] )
 b = np.ndarray( [num_events] )
 for i in range(len(a)):
-   a[i] = Data[i]/Y[i]
+   a[i] = Data[i]
    b[i] = Y[i]
-   Eprof.Fill(b[i], a[i])
-Eprof.Draw()
+gr  = TGraph( num_events, b, a)
+gr.SetTitle("Ecal versus Ep ; Primary Energy GeV ;Total Energy Deposited in Ecal GeV/50")
+gr.Draw('ATP')
+gr.Fit("pol2")
 c1.Update()
-Eprof.Fit("pol2")
+c1.Print("pol2Fit.pdf")
+gr.Fit("pol3")
 c1.Update()
-c1.Print("prof2Fit.pdf")
-Eprof.Fit("pol3")
+c1.Print("pol3Fit.pdf")
+gr.Fit("pol4")
 c1.Update()
-c1.Print("prof3Fit.pdf")
-Eprof.Fit("pol4")
+c1.Print("pol4Fit.pdf")
+gr.Fit("gaus")
 c1.Update()
-c1.Print("prof4Fit.pdf")
-Eprof.Fit("gaus")
-c1.Print("profguassFit.pdf")
-Eprof.Fit("expo")
+c1.Print("guassFit.pdf")
+gr.Fit("expo")
 c1.Update()
-c1.Print("profexpoFit.pdf")
+c1.Print("expoFit.pdf")
 
 # request user action before ending (and deleting graphics window)
 raw_input('Press <ret> to end -> ')
