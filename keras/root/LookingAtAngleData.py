95afaceffd9021107f5be28e8f5b5347e84b0f2c
Gul Rukh Khattak <gkhattak@rudra.dyndns.cern.ch>
2018-04-25 13:11:07 +0200

    Colored plots with axis names

--- keras/root/LookingAtAngleData.py
+++ keras/root/LookingAtAngleData.py
@@ -9,11 +9,13 @@ import time
 import glob
 import numpy.core.umath_tests as umath
 import GANutils as gan
+import ROOTutils as r
  
 def main():
    #datapath = '/bigdata/shared/LCD/NewV1/*scan/*.h5' #Training data path
    datapath = "/eos/project/d/dshep/LCD/DDHEP/*scan_RandomAngle_*_MERGED/*Escan_RandomAngle_*.h5"
-   numdata = 10000
+   numdata = 100000
+   numevents = 1000
    energies=[10, 250, 400]
    phis = [2, 10, 18]
    thetas = [65, 90, 118]
@@ -24,16 +26,19 @@ def main():
    plotsdir = 'angle_plots'
    gan.safe_mkdir(plotsdir)
    x, y, theta, phi, angle= GetAllData(datafiles)
-   yvar = gan.sort([x, y], energies, num_events=2000)
-   thetavar = gan.sort([x, theta], thetas, num_events=2000, tolerance=2)
-   phivar = gan.sort([x, phi], phis, num_events=2000, tolerance=2)
+   yvar = gan.sort([x, y], energies, num_events=numevents)
+   thetavar = gan.sort([x, theta], thetas, num_events=numevents, tolerance=2)
+   phivar = gan.sort([x, phi], phis, num_events=numevents, tolerance=2)
    for energy in energies:
       for xcut, ycut, zcut in zip(xcuts, ycuts, zcuts):
          PlotCut(yvar["events_act" + str(energy)], xcut, ycut, zcut, energy, os.path.join(plotsdir, '2Dhits_cut{}_{}.pdf'.format(xcut, energy)))
+         print('The number of events in {} GeV bin is {}.'.format(energy, yvar["events_act" + str(energy)].shape[0]))
    for t in thetas:
       PlotAngleCut(thetavar["events_act" + str(t)], t, os.path.join(plotsdir, 'Theta_cut{}.pdf'.format(t)))
+      print('The number of events in {} theta bin is {}.'.format(t, thetavar["events_act" + str(t)].shape[0]))
    for p in phis:
       PlotAngleCut(phivar["events_act" + str(p)], p, os.path.join(plotsdir, 'Phi_cut{}.pdf'.format(p)))
+      print('The number of events in {} phi bin is {}.'.format(p, phivar["events_act" + str(p)].shape[0])) 
 
 def PlotCut(events, xcut, ycut, zcut, energy, out_file):
    canvas = TCanvas("canvas" ,"Data 2D Hist" ,200 ,10 ,700 ,500) #make
@@ -41,23 +46,31 @@ def PlotCut(events, xcut, ycut, zcut, energy, out_file):
    hx = ROOT.TH2F('x_{}GeV_x={}cut'.format(str(energy), str(xcut)), '', 51, 0, 51, 25, 0, 25)
    hy = ROOT.TH2F('y_{}GeV_y={}cut'.format(str(energy), str(ycut)), '', 51, 0, 51, 25, 0, 25)
    hz = ROOT.TH2F('z_{}GeV_z={}cut'.format(str(energy), str(zcut)), '', 51, 0, 51, 51, 0, 51)
+   gStyle.SetPalette(1)
+   gPad.SetLogz()
    FillHist2D_wt(hx, events[:, xcut, :, :])
    FillHist2D_wt(hy, events[:, :, ycut, :])
    FillHist2D_wt(hz, events[:, :, :, zcut])
    canvas.cd(1)
-   hx.Draw()
+   hx.Draw("colz")
    hx.GetXaxis().SetTitle("Y axis")
    hx.GetYaxis().SetTitle("Z axis")
    canvas.Update()
+   r.stat_pos(hx)
+   canvas.Update()
    canvas.cd(2)
-   hy.Draw()
+   hy.Draw("colz")
    hy.GetXaxis().SetTitle("X axis")
    hy.GetYaxis().SetTitle("Z axis")
    canvas.Update()
+   r.stat_pos(hy)
+   canvas.Update()
    canvas.cd(3)
-   hz.Draw()
+   hz.Draw("colz")
    hz.GetXaxis().SetTitle("X axis")
    hz.GetYaxis().SetTitle("Y axis")
+   canvas.Update()
+   r.stat_pos(hz)
    canvas.Update()
    canvas.Print(out_file)
 
@@ -71,19 +84,25 @@ def PlotAngleCut(events, ang, out_file):
    FillHist2D_wt(hy, events[:, :, 25, :])
    FillHist2D_wt(hz, events[:, :, :, 12])
    canvas.cd(1)
-   hx.Draw()
+   hx.Draw("colz")
    hx.GetXaxis().SetTitle("Y axis")
    hx.GetYaxis().SetTitle("Z axis")
    canvas.Update()
+   r.stat_pos(hx)
+   canvas.Update()
    canvas.cd(2)
-   hy.Draw()
+   hy.Draw("colz")
    hy.GetXaxis().SetTitle("X axis")
-   hy.GetXaxis().SetTitle("Z axis")
+   hy.GetYaxis().SetTitle("Z axis")
+   canvas.Update()
+   r.stat_pos(hy)
    canvas.Update()
    canvas.cd(3)
-   hz.Draw()
+   hz.Draw("colz")
    hz.GetXaxis().SetTitle("X axis")
-   hz.GetXaxis().SetTitle("Y axis")
+   hz.GetYaxis().SetTitle("Y axis")
+   canvas.Update()
+   r.stat_pos(hz)
    canvas.Update()
    canvas.Print(out_file)
 
@@ -95,11 +114,14 @@ def FillHist2D_wt(hist, array):
    bin1 = np.arange(dim1)
    bin2 = np.arange(dim2)
    bin3 = np.arange(dim3)
+   count = 0
    for j in bin2:
      for k in bin3:
         for i in bin1:
             hist.Fill(j, k, array[i, j, k])
-   hist.Sumw2()
+            count+=1
+   print('For {} events with {} xbins and {} ybins the countis {}'.format(dim1, dim2, dim3, count))
+   #hist.Sumw2()
 
 def GetAngleData(datafile):
     #get data for training
