3f6df5446bae2cc3fe0fe330bbbb9b562fbdb89e
gulrukhk <gulrukhk@yahoo.com>
2018-08-28 03:49:00 -0700

    approx angle measurement

--- archives/EnergyAngle/EcalCondGan.py
+++ archives/EnergyAngle/EcalCondGan.py
@@ -12,36 +12,72 @@ from keras.layers.convolutional import (UpSampling3D, Conv3D, ZeroPadding3D,
                                         AveragePooling3D)
 
 from keras.models import Model, Sequential
+import math
+from tensorflow import py_func, float32, Tensor
+import tensorflow as tf
 
 K.set_image_dim_ordering('tf')
 
+def ecal_sum(image):
+    sum = K.sum(image, axis=(1, 2, 3))
+    #sum = K.expand_dims(sum)
+    return sum
+   
+def ecal_angle(image):
+    a = K.sum(image, axis=(1, 4))
+    maxa = K.argmax(a, axis=1)
+    b = np.arange(25)
+    p = []
+    ang = []
+    for i in np.arange(image.shape[0]):    
+       p.append( np.polyfit(b, maxa[i], 1))
+       ang.append( math.atan(p[i][0]))
+    ang = tf.convert_to_tensor(ang, dtype=tf.float32)
+    return ang
+    
+def ecal_angle2(image):
+    a = K.sum(image, axis=(1, 4))
+    print 'evenst', K.int_shape(a)
+    b = K.argmax(a, axis=1)
+    print 'argmax', K.int_shape(b)
+    c = K.cast(b[:,21] - b[:,3], dtype='float32')/18.0
+    print 'tan', K.int_shape(c)
+    d = tf.atan(c)
+    d = (3.14158/2.0) - d
+    print 'angle', K.int_shape(d)
+    #d = K.expand_dims(d)
+    return d
+
+def tf_ecal_angle(image):
+    out = py_func(ecal_angle2, [image], float32)
+    return out
+
+def output_of_lambda(input_shape):
+    return (input_shape[0], 1)
+
 def discriminator():
+  
+    image=Input(shape=(51, 51, 25, 1))
 
-    image = Input(shape=(51, 51, 25, 1))
-
-    x = Conv3D(16, 6, 8, 6, border_mode='same')(image)
+    x = Conv3D(16, 5, 6, 6, border_mode='same')(image)
     x = LeakyReLU()(x)
     x = Dropout(0.2)(x)
 
     x = ZeroPadding3D((0, 0, 1))(x)
-    x = Conv3D(8, 5, 8, 6, border_mode='valid')(x)
+    x = Conv3D(8, 5, 6, 6, border_mode='valid')(x)
     x = LeakyReLU()(x)
     x = BatchNormalization()(x)
     x = Dropout(0.2)(x)
 
     x = ZeroPadding3D((0, 0, 1))(x)
-    x = Conv3D(8, 8, 8, 6, border_mode='valid')(x)
+    x = Conv3D(8, 5, 6, 6, border_mode='valid')(x)
     x = LeakyReLU()(x)
     x = BatchNormalization()(x)
     x = Dropout(0.2)(x)
 
-    x = ZeroPadding3D((0, 0, 1))(x)
-    x = Conv3D(8, 8, 8, 6, border_mode='valid')(x)
-    x = LeakyReLU()(x)
-    x = BatchNormalization()(x)
-    x = Dropout(0.2)(x)
-
-    x = Conv3D(8, 8, 8, 6, border_mode='valid')(x)
+    #x = AveragePooling3D((2, 2, 2))(x)
+    #x = ZeroPadding3D((1, 1, 1))(x)
+    x = Conv3D(8, 5, 6, 6, border_mode='valid')(x)
     x = LeakyReLU()(x)
     x = BatchNormalization()(x)
     x = Dropout(0.2)(x)
@@ -56,38 +92,34 @@ def discriminator():
 
     fake = Dense(1, activation='sigmoid', name='generation')(dnn_out)
     aux = Dense(1, activation='linear', name='auxiliary')(dnn_out)
-    eta = Dense(1, activation='linear', name='eta')(dnn_out)
+    #theta = Dense(1, activation='linear', name='theta')(dnn_out)
     #phi = Dense(1, activation='linear', name='phi')(dnn_out)
+    theta = Lambda(ecal_angle2)(image)
+    ecal = Lambda(ecal_sum)(image)
+    Model(input=image, output=[fake, aux, theta, ecal]).summary()
+    return Model(input=image, output=[fake, aux, theta, ecal])
 
-    ecal = Lambda(lambda x: K.sum(x, axis=(1, 2, 3)))(image)
-    Model(input=image, output=[fake, aux, eta, ecal]).summary()
-    return Model(input=image, output=[fake, aux, eta, ecal])
 
-def generator(latent_size=256, return_intermediate=False):
+def generator(latent_size=200, return_intermediate=False):
     
     loc = Sequential([
         Dense(2592, input_shape=(2,latent_size)),
         Reshape((9, 9, 8, 8)),
 
-        Conv3D(64, 6, 8, 8, border_mode='same', init='he_uniform'),
+        Conv3D(64, 6, 6, 8, border_mode='same', init='he_uniform'),
         LeakyReLU(),
         BatchNormalization(),
         UpSampling3D(size=(3, 3, 2)),
 
-        ZeroPadding3D((2, 2, 1)),
-        Conv3D(6, 5, 6, 8, init='he_uniform'),
+        ZeroPadding3D((2, 3, 1)),
+        Conv3D(6, 5, 8, 8, init='he_uniform'),
         LeakyReLU(),
         BatchNormalization(),
         UpSampling3D(size=(2, 2, 3)),
 
-        ZeroPadding3D((0, 2, 1)),
-        Conv3D(6, 3, 5, 7, init='he_uniform'),
+        ZeroPadding3D((0, 2,0)),
+        Conv3D(6, 3, 5, 8, init='he_uniform'),
         LeakyReLU(),
-
-        ZeroPadding3D((1, 1, 1)),
-        Conv3D(6, 3, 3, 6, init='he_uniform'),
-        LeakyReLU(),
-
         Conv3D(1, 2, 2, 2, bias=False, init='glorot_normal'),
         Activation('relu')
     ])
@@ -99,4 +131,3 @@ def generator(latent_size=256, return_intermediate=False):
 
 g= generator()
 d=discriminator()
-
