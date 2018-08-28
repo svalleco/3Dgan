eccdb035f9e08deec4932a1a50e785374c5e2038
gulrukhk <gulrukhk@yahoo.com>
2018-08-28 03:52:11 -0700

    angle concatenated to latent

--- archives/EnergyAngle/EcalCondGan.py
+++ archives/EnergyAngle/EcalCondGan.py
@@ -18,13 +18,12 @@ K.set_image_dim_ordering('tf')
 def discriminator():
 
     image = Input(shape=(51, 51, 25, 1))
-    phi = Input(shape=(1,))
-    
+
     x = Conv3D(8, 6, 8, 6, border_mode='same')(image)
     x = LeakyReLU()(x)
     x = Dropout(0.2)(x)
 
-    #x = ZeroPadding3D((0, 0, 1))(x)
+    x = ZeroPadding3D((0, 0, 1))(x)
     x = Conv3D(8, 5, 8, 6, border_mode='valid')(x)
     x = LeakyReLU()(x)
     x = BatchNormalization()(x)
@@ -49,25 +48,25 @@ def discriminator():
 
     x = AveragePooling3D((2, 2, 2))(x)
     h = Flatten()(x)
-  
+
     dnn = Model(image, h)
     dnn.summary()
 
-    #dnn_out = dnn(image)
-    dnn_out = merge([dnn(image), phi], mode = 'concat')
+    dnn_out = dnn(image)
+
     fake = Dense(1, activation='sigmoid', name='generation')(dnn_out)
     aux = Dense(1, activation='linear', name='auxiliary')(dnn_out)
-    eta = Dense(1, activation='linear', name='eta')(dnn_out)
-    #phi = Dense(1, activation='linear', name='phi')(dnn_out)
+    #eta = Dense(1, activation='linear', name='eta')(dnn_out)
+    theta = Dense(1, activation='linear', name='theta')(dnn_out)
 
     ecal = Lambda(lambda x: K.sum(x, axis=(1, 2, 3)))(image)
-    Model(input=[image, phi], output=[fake, aux, eta, ecal]).summary()
-    return Model(input=[image, phi], output=[fake, aux, eta, ecal])
+    Model(input=image, output=[fake, aux, theta, ecal]).summary()
+    return Model(input=image, output=[fake, aux, theta, ecal])
 
 def generator(latent_size=256, return_intermediate=False):
     
     loc = Sequential([
-        Dense(1728, input_shape=(3,latent_size)),
+        Dense(5184, input_shape=(latent_size, )),
         Reshape((9, 9, 8, 8)),
 
         Conv3D(64, 6, 8, 8, border_mode='same', init='he_uniform'),
@@ -92,7 +91,7 @@ def generator(latent_size=256, return_intermediate=False):
         Conv3D(1, 2, 2, 2, bias=False, init='glorot_normal'),
         Activation('relu')
     ])
-    latent = Input(shape=(3, latent_size ))   
+    latent = Input(shape=(latent_size, ))   
     fake_image = loc(latent)
     loc.summary()
     Model(input=[latent], output=fake_image).summary()
