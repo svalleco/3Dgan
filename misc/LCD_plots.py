43c4d4bf4c0850483321f32fce280b53eaeff230
gulrukhk <gulrukhk@yahoo.com>
2018-09-12 02:04:38 -0700

    plots for LCD group paper

---
+++ misc/LCD_plots.py
@@ -0,0 +1,273 @@
+import h5py
+import numpy as np
+import setGPU
+import math
+import sys
+import ROOT
+import os
+sys.path.insert(0,'/nfshome/gkhattak/3Dgan/')
+sys.path.insert(0,'/nfshome/gkhattak/3Dgan/analysis')
+
+from utils.GANutils import perform_calculations_angle, safe_mkdir  # to calculate different Physics quantities
+import utils.ROOTutils as my # common utility functions for root
+
+def main():
+    #Architecture
+    from AngleArch3dGAN_sqrt import generator, discriminator
+
+    #Weights
+    disc_weight1="../weights/3Dweights_1loss_50weight_sqrt/params_discriminator_epoch_040.hdf5"
+    gen_weight1= "../weights/3Dweights_1loss_50weight_sqrt/params_generator_epoch_040.hdf5"
+
+    #Path to store results
+    plotsdir = "results/LCD_paper_plots_w50/"
+    safe_mkdir(plotsdir)
+    #Parameters
+    latent = 256 # latent space
+    num_data = 100000
+    num_events = 1000
+    events_per_file = 5000
+    m = 3  # number of moments
+    nloss= 4 # total number of losses...4 or 5
+    concat = 1 # if concatenting angle to latent space
+    cell=1 # if making plots for cell energies. Exclude for quick plots.
+    corr=0 # if making correlation plots
+    energies=[0] #, 110, 150, 190] # energy bins
+    thetas = [math.radians(x) for x in [90, 62, 118]] # angle bins
+    aindexes = [0, 1, 2] # numbers corressponding to different angle bins
+    angtype = 'theta'# the angle data to be read from file
+    particle='Ele'# partcile type
+    thresh=0 # Threshold for ecal energies
+    datapath = "/data/shared/gkhattak/*Measured3ThetaEscan/*.h5"  # Data path
+
+    sortdir = 'SortedAngleData'  # if saving sorted data
+    gendir = 'SortedAngleGen'  # if saving generated events
+    discdir = 'SortedAngleDisc' # if saving disc outputs
+
+    Test = True # use test data
+    stest = False # K and chi2 test
+
+    #following flags are used to save sorted and GAN data and to load from sorted data. These are used while development and should be False for one time analysis
+    save_data = False # True if the sorted data is to be saved. It only saves when read_data is false
+    read_data = False # True if loading previously sorted data
+    save_gen =  False # True if saving generated data.
+    read_gen = False # True if generated data is already saved and can be loaded
+    save_disc = False # True if discriminiator data is to be saved
+    read_disc =  False # True if discriminated data is to be loaded from previously saved file
+    ifpdf = True # True if pdf are required. If false .C files will be generated
+
+    flags =[Test, save_data, read_data, save_gen, read_gen, save_disc, read_disc]
+    dweights = [disc_weight1]#, disc_weight2]
+    gweights = [gen_weight1]#, gen_weight2]
+    xscales = [1]#, 1]
+    ascales = [1]#, 1]
+    labels = ['']#, 'epoch 40']
+    d = discriminator()
+    g = generator(latent)
+    var= perform_calculations_angle(g, d, gweights, dweights, energies, thetas,
+                                    aindexes, datapath, sortdir, gendir, discdir, num_data, num_events, m, xscales,
+                                    ascales, flags, latent, events_per_file, particle, thresh=thresh, angtype=angtype, offset=0.0,
+                                    nloss=nloss, concat=concat
+                                    , pre =sqrt, post =square  # Adding other preprocessing, Default is simple scaling
+                                    )
+    for energy in energies:
+        sumx_act=[]
+        sumy_act=[]
+        sumz_act=[]
+        sumx_gan=[]
+        sumy_gan=[]
+        sumz_gan=[]
+        ecal_act=[]
+        ecal_gan=[]
+        energy_act =[]
+        
+        for i, theta in enumerate(thetas):
+           print(var["ecal_act"+ str(energy) + "ang_" + str(i)]) 
+           for key in var["ecal_act"+ str(energy) + "ang_" + str(i)]:
+    
+               print(key)
+           sumx_act.append(var["sumsx_act"+ str(energy) + "ang_" + str(i)])
+           sumy_act.append(var["sumsy_act"+ str(energy) + "ang_" + str(i)])
+           sumz_act.append(var["sumsz_act"+ str(energy) + "ang_" + str(i)])
+           sumx_gan.append(var["sumsx_gan"+ str(energy) + "ang_" + str(i)]['n_0'])
+           sumy_gan.append(var["sumsy_gan"+ str(energy) + "ang_" + str(i)]['n_0'])
+           sumz_gan.append(var["sumsz_gan"+ str(energy) + "ang_" + str(i)]['n_0'])
+           ecal_act.append(var["ecal_act"+ str(energy) + "ang_" + str(i)])
+           ecal_gan.append(var["ecal_gan"+ str(energy) + "ang_" + str(i)])
+           energy_act.append(var["energy"+ str(energy) + "ang_" + str(i)])                                         
+        PlotEnergyHistY(sumy_act, sumy_gan, os.path.join(plotsdir, 'angle_histy3_{}_0'.format(energy)), energy, thetas)
+        PlotEnergyHistY(sumy_act, sumy_gan, os.path.join(plotsdir, 'angle_histy3_log{}_0'.format(energy)), energy, thetas, log=1)
+        #plot_ecal_ratio_profile(ecal_act, ecal_gan, energy_act, thetas, os.path.join(plotsdir, 'ecal_{}'.format(energy)), p=[100, 200], ifpdf=True)
+
+def sqrt(n, scale=1):
+    return np.sqrt(n * scale)
+
+def square(n, scale=1):
+    return np.square(n)/scale
+
+def PlotEnergyHistY(sumy_act, sumy_gan, out_file, energy, thetas, log=0, ifC=False):
+    canvas = ROOT.TCanvas("canvas" ,"abc" ,200 ,10 ,700 ,500) #make
+    canvas.SetGrid()
+    if log:
+       ROOT.gPad.SetLogy()
+    label = "Weighted Histograms for {} GeV".format(energy)
+    color = 2
+    leg = ROOT.TLegend(0.7,0.6,0.9,0.9)
+    #leg.SetTextSize(0.05)
+    h1y=[]
+    h2y=[]
+    for i, theta in enumerate(thetas):
+        theta = int(np.degrees(theta))
+        num = sumy_act[i].shape[0]
+        print(theta, num)
+        y=sumy_act[0].shape[1]
+        h1y.append(ROOT.TH1F('G4y{:d}theta_{:d}GeV'.format(theta, energy), '', y, 0, y))
+        h2y.append(ROOT.TH1F('GANy{:d}theta_{:d}GeV'.format(theta, energy), '', y, 0, y))
+    
+        h1y[i].SetLineColor(color)
+        h2y[i].SetLineColor(color)
+        h1y[i].SetLineWidth(2)  
+        my.fill_hist_wt(h1y[i], sumy_act[i])
+        h2y[i].SetLineStyle(2)
+        
+        my.fill_hist_wt(h2y[i], sumy_gan[i])
+        h1y[i].SetStats(0)
+        h2y[i].SetStats(0)
+        h1y[i]=my.normalize(h1y[i], 1)
+        h2y[i]=my.normalize(h2y[i], 1)
+        if i==0:
+           h1y[i].GetXaxis().SetTitle("Energy deposition along Y axis")
+           h1y[i].GetYaxis().SetTitle("count/integral")
+           h1y[i].GetYaxis().CenterTitle()
+           #h1y[i].GetYaxis().SetRangeUser(0, 1.2)
+        
+        if i ==0:
+            h1y[i].Draw('')
+            h1y[i].Draw('sames hist')
+            canvas.Update()
+        else:
+            h1y[i].Draw('sames')
+            h1y[i].Draw('sames hist')
+        h2y[i].Draw('sames')
+        h2y[i].Draw('sames hist')
+                    
+        leg.AddEntry(h1y[i], 'G4   {}\circ'.format(theta),"l")
+        leg.AddEntry(h2y[i], 'GAN {}\circ'.format(theta),"l")
+        
+        color+=2
+        canvas.Update()
+    leg.Draw()
+    canvas.Update()
+    canvas.Print(out_file + '.pdf')
+    if ifC:
+       canvas.Print(out_file + '.C')
+                                                                     
+def PlotEnergyHistY2(sumy_act, sumy_gan, out_file, energy, thetas, log=0, ifC=False):
+    canvas = ROOT.TCanvas("canvas" ,"abc" ,200 ,10 ,700 ,500) #make
+    canvas.SetGrid()
+    label = "Weighted Histograms for {} GeV".format(energy)
+    leg = ROOT.TLegend(0.1,0.6,0.9,0.9)
+    canvas.Divide(2,2)
+    #leg.SetTextSize(0.05)
+    h1y=[]
+    h2y=[]
+    if len(thetas)>3:
+        thetas=thetas[:3]
+        print('Histogram will only be plotted for {:.2f} degrees'.format(thetas))
+        
+    for i, theta in enumerate(thetas):
+        theta = int(np.degrees(theta))
+        num = sumy_act[i].shape[0]
+        canvas.cd(i+1)
+        if log:
+            ROOT.gPad.SetLogy()
+                    
+        y=sumy_act[0].shape[1]
+        h1y.append(ROOT.TH1F('G4y{:d}theta_{:d}GeV'.format(theta, energy), '', y, 0, y))
+        h2y.append(ROOT.TH1F('GANy{:d}theta_{:d}GeV'.format(theta, energy), '', y, 0, y))
+
+        h1y[i].SetLineColor(2)
+        h2y[i].SetLineColor(4)
+
+        my.fill_hist_wt(h1y[i], sumy_act[i])
+        #h2y[i].SetLineStyle(2)
+
+        my.fill_hist_wt(h2y[i], sumy_gan[i])
+        h1y[i].SetStats(0)
+        h2y[i].SetStats(0)
+        h1y[i]=my.normalize(h1y[i], 1)
+        h2y[i]=my.normalize(h2y[i], 1)
+        h1y[i].GetXaxis().SetTitle("Energy deposition along Y axis for {} degrees".format(int(theta)))
+        h1y[i].GetYaxis().SetTitle("count/integral")
+        h1y[i].GetYaxis().CenterTitle()
+        h1y[i].GetYaxis().SetTitleOffset()
+        h1y[i].GetYaxis().SetTitleSize(0.05)
+        h1y[i].GetXaxis().SetTitleSize(0.045)
+        h1y[i].GetXaxis().SetTitleOffset()
+        #h1y[i].GetYaxis().SetRangeUser(0, 1.2)
+        
+        h1y[i].Draw('')
+        h1y[i].Draw('sames hist')
+        canvas.Update()
+        
+        h2y[i].Draw('sames')
+        h2y[i].Draw('sames hist')
+        canvas.Update()
+    canvas.cd(4)
+    leg.AddEntry(h1y[i], 'G4',"l")
+    leg.AddEntry(h2y[i], 'GAN',"l")
+              
+    leg.Draw()
+    canvas.Update()
+    canvas.Print(out_file + '.pdf')
+    if ifC:
+        canvas.Print(out_file + '.C')
+                                                           
+# PLot ecal ratio
+def plot_ecal_ratio_profile(ecal1, ecal2, y, thetas, out_file, p=[100, 200], ifpdf=True):
+    canvas = ROOT.TCanvas("c1" ,"" ,200 ,10 ,700 ,500) #make
+    canvas.SetGrid()
+    canvas.Divide(2,2)
+    Eprofs=[]
+    Gprofs=[]
+    legend = ROOT.TLegend(.1, .8, .9, .9)
+    for i, theta in enumerate(thetas):
+        canvas.cd(i+1)
+        Eprofs.append(ROOT.TProfile("Eprof" + str(i), "Eprof" + str(i), 100, int(p[0]), int(p[1])))
+        Gprofs.append(ROOT.TProfile("Gprof" + str(i), "Gprof" + str(i), 100, int(p[0]), int(p[1])))
+        Eprof=Eprofs[i]
+        Eprof.SetStats(ROOT.kFALSE)
+        my.fill_profile(Eprof, ecal1[i]/y[i], y[i])
+        Eprof.GetXaxis().SetTitle("Ep GeV for theta={} degrees".format(int(theta)))
+        Eprof.GetYaxis().SetTitle("50 x Ecal/Ep")
+        Eprof.GetYaxis().CenterTitle()
+        Eprof.GetYaxis().SetRangeUser(0, 2.5)
+        Eprof.Draw()
+        Eprof.SetLineColor(2)
+                        
+        Gprof = Gprofs[i]
+        Gprof.SetStats(ROOT.kFALSE)
+        my.fill_profile(Gprof, ecal2[i]/y[i], y[i])
+        Gprof.SetLineColor(4)
+        Gprof.Draw('sames')
+        c1.Update()
+    canvas.cd(4)
+    legend.AddEntry(Eprof, "G4", "l")
+    legend.AddEntry(Gprof, "GAN", "l")
+    legend.Draw()
+    c1.Modified()
+    c1.Update()
+    if ifpdf:
+        c1.Print(out_file + '.pdf')
+    else:
+        c1.Print(out_file + '.C')
+                                                                                                                                                                              
+        
+if __name__ == "__main__":
+    main()
+        
+
+                                                                      
+
+                                                                                                                                                    
+                            
