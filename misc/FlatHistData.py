db419afff9ee9f96f4fe89a00fde0040ba08ec6a
gulrukhk <gulrukhk@yahoo.com>
2018-09-19 01:29:42 -0700

    Correcting energy range

--- misc/FlatHistData.py
+++ misc/FlatHistData.py
@@ -11,58 +11,59 @@ import sys
 import numpy.core.umath_tests as umath
 sys.path.insert(0,'/nfshome/gkhattak/3Dgan/analysis')
 sys.path.insert(0,'/nfshome/gkhattak/3Dgan')
+sys.path.insert(0,'/nfshome/gkhattak/keras/architectures_tested/')
 import utils.GANutils as gan
 import utils.ROOTutils as r
 import setGPU
 
 def main():
    datapath = "/data/shared/gkhattak/EleMeasured3ThetaEscan/Ele_VarAngleMeas_100_200_000.h5"
-   genweight1 = "/nfshome/gkhattak/3Dgan/weights/params_generator_epoch_032.hdf5"
-   #genweight2 = "/nfshome/gkhattak/3Dgan/weights/3Dweights_1loss_25weight_sqrt/params_generator_epoch_010.hdf5"
-   #genweight3 = "/nfshome/gkhattak/3Dgan/weights/3Dweights_1loss_25weight_sqrt/params_generator_epoch_020.hdf5"
-   genweight4 = "/nfshome/gkhattak/3Dgan/weights/3Dweights_1loss_25weight/params_generator_epoch_040.hdf5"
-      
+   #datapath2 = "/data/shared/LCDLargeWindow/fixedangle/EleEscan/EleEscan_1_1.h5"
+   #datapath3 = '/bigdata/shared/LCD/NewV1/EleEscan/EleEscan_1_1.h5'
+   genweight = "/nfshome/gkhattak/3Dgan/weights/3Dweights_100/params_generator_epoch_001.hdf5"
+   #genweight2 = "/nfshome/gkhattak/keras/weights/scaled100_2p1p1_weights/params_generator_epoch_042.hdf5"
+   #genweight3 = "/nfshome/gkhattak/3Dgan/weights/3Dweights_1loss_25weight_sqrt/params_generator_epoch_025.hdf5"
    # generator model
-   from AngleArch3dGAN_sqrt import generator as g1
-   from AngleArch3dGAN import generator as g2
+   from AngleArch3dGAN import generator
+   #from EcalEnergyGan import generator as generator2
+   #from AngleArch3dGAN import generator3
 
    numdata = 1000
-   scale=10
-   outdir = 'sqrt_scale10_5'
+   scale=100
+   outdir = 'results/var_scale100_ep1'
    gan.safe_mkdir(outdir)
    outfile = os.path.join(outdir, 'Ecal')
    x, y, ang=GetAngleData(datapath, numdata)
+   print('The angle data varies from {} to {}'.format(np.amin(x[x>0]), np.amax(x)))
+   #x2, y2=GetData(datapath2, numdata, thresh=0)
+   #print('The fixed data varies from {} to {}'.format(np.amin(x2[x2>0]), np.amax(x2)))
+   #x3, y3=GetData2(datapath3, numdata, thresh=0)
+   #print('The fixed data varies from {} to {}'.format(np.amin(x3[x3>0]), np.amax(x3)))
    
    latent = 256 # latent space for generator
-   g=g1(latent) # build generator
-   g.load_weights(genweight1) # load weights        
+   g=generator(latent) # build generator
+   g.load_weights(genweight) # load weights        
    x_gen1 = gan.generate(g, numdata, [y/100, ang], latent)
-   x_gen2 = postproc(x_gen1 , np.square, 10)
-
-   g = g2(latent)
-   g.load_weights(genweight4) # load weights
-   x_gen3 = gan.generate(g, numdata, [y/100, ang], latent)
+   x_gen2 = x_gen1/scale
+   
    """
+   latent = 200 # latent space for generator
+   g2.load_weights(genweight2) # load weights
+   x_gen3 = gan.generate(g2, numdata, [y/100], latent)
+   x_gen4 = x_gen3/100
+   
    g.load_weights(genweight3) # load weights
    x_gen3 = gan.generate(g, numdata, [y/100, ang], latent)
-      
-   g.load_weights(genweight4) # load weights
-   x_gen4 = gan.generate(g, numdata, [y/100, ang], latent)
-   """   
-   labels = ['G4 ', 'GAN sqrt', 'GAN sqrt(square)', 'GAN without sqrt'] #, 'GAN epoch 10', 'GAN epoch 20', 'GAN epoch 40']
-   plot_ecal_flatten_hist([x, x_gen1, x_gen2, x_gen3], outfile, y, labels, norm=1)
-   plot_ecal_flatten_hist([x, x_gen1, x_gen2, x_gen3], outfile + '_log', y, labels, logy=1, norm=1)
+   x_gen4 = postproc(x_gen3 , np.square, scale)
+   """
+   labels = ['G4', 'GAN raw(scale 100)', 'GAN/100'] #'GAN var raw', 'GAN var (sqrt)', 'GAN fixed raw', 'GAN fixed scaled'] 
+   #plot_ecal_flatten_hist([x, x2, x_gen1, x_gen2, x_gen3, x_gen4], outfile, y, labels, norm=1)
+   #plot_ecal_flatten_hist([x, x2, x_gen1, x_gen2, x_gen3, x_gen4], outfile + '_log', y, labels, logy=1, norm=1)
+   plot_ecal_flatten_hist([x, x_gen1, x_gen2], outfile + '_log', y, labels, logy=1, norm=2)
    print('Histogram is saved in ', outfile)
        
 def postproc(event, f, scale):
    return f(event)/scale
-
-def sqrt(x):
-   epsilon= np.finfo(float).eps
-   indexes = np.where(x>0)
-   x[indexes] = np.sqrt(x[indexes])
-   return x
-                
 
 def GetAngleData(datafile, numevents, ftn=0, scale=1, angtype='theta'):
    #get data for training
@@ -75,20 +76,41 @@ def GetAngleData(datafile, numevents, ftn=0, scale=1, angtype='theta'):
    ang = np.array(f.get(angtype)[:numevents])
    return x, y, ang
 
+def GetData(datafile, numevents, scale=1, thresh=1e-6):
+   #get data for training
+   print 'Loading Data from .....', datafile
+   f=h5py.File(datafile,'r')
+   y=np.array(f.get('energy')[:numevents])
+   x=np.array(f.get('ECAL')[:numevents])
+   x[x<thresh] = 0
+   x = x * scale
+   return x, y
+                              
+def GetData2(datafile, numevents, scale=1, thresh=1e-6):
+   #get data for training
+   print 'Loading Data from .....', datafile
+   f=h5py.File(datafile,'r')
+   y=np.array(f.get('target')[:numevents, 1])
+   x=np.array(f.get('ECAL')[:numevents])
+   x[x<thresh] = 0
+   x = x * scale
+   return x, y
+                     
+
 def plot_ecal_flatten_hist(events, out_file, energy, labels, logy=0, norm=0, ifpdf=True):
    c1 = ROOT.TCanvas("c1" ,"" ,200 ,10 ,700 ,500) #make
    c1.SetGrid()
    ROOT.gPad.SetLogx()
    
-   title = "Ecal Flat Histogram for 2-500 GeV"
+   title = "Cell energy deposits for 100-200 GeV "
    legend = ROOT.TLegend(.1, .6, .3, .9)
    color =2
    if logy:
       ROOT.gPad.SetLogy()
-      title = title + " (y scale in log)"
+      title = title + " (log)"
    hds=[]
    for i, (event, label) in enumerate(zip(events, labels)):
-      hds.append(ROOT.TH1F(label, "", 100, -12, 2))
+      hds.append(ROOT.TH1F(label, "", 100, -12, 4))
       hd = hds[i]
       hd.SetStats(0)
       r.BinLogX(hd)
