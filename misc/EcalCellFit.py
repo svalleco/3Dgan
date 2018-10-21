0c2df5d4fa034001b94e28647fa8e210ce43f560
gulrukhk <gulrukhk@yahoo.com>
2018-10-20 19:03:24 -0700

    Testing fits for ecal cell energies

--- misc/EcalCellFit.py
+++ misc/EcalCellFit.py
@@ -21,17 +21,17 @@ def main():
     datafile = "/data/shared/gkhattak/EleMeasured3ThetaEscan/Ele_VarAngleMeas_100_200_000.h5"
     numevents= 1000
     thresh = 0
-    dirname = 'results/ecal_flat'
+    dirname = 'results/ecal_flat_rd_range_fit'
     gan.safe_mkdir(dirname)
-    fits = ['landau','gaus', 'expo']
+    fits = ['landau','gaus', 'expon']
     #log= False #take log of data
     x, y =GetData(datafile, numevents, thresh=thresh)
-    plot_ecal_flatten(x, os.path.join(dirname, 'ecal_flat'), fits, 'G4', norm=1, logy=0, logx=0)
-    plot_ecal_flatten(x, os.path.join(dirname, 'ecal_flat_logy'), fits, 'G4', norm=1, logy=1, logx=0)
-    plot_ecal_flatten(x, os.path.join(dirname, 'ecal_flat_logx'), fits, 'G4', norm=1, logy=0, logx=1)
-    plot_ecal_flatten(x, os.path.join(dirname, 'ecal_flat_logx_logy'), fits, 'G4', norm=1, logy=1, logx=1)
-    plot_ecal_flatten(x, os.path.join(dirname, 'ecal_flat_logbins'), fits, 'G4', norm=1, logy=1, logx=1, logbin=1)
-
+    #plot_ecal_flatten(x, os.path.join(dirname, 'ecal_flat'), fits, 'G4', norm=1, logy=0, logx=0)
+    #plot_ecal_flatten(x, os.path.join(dirname, 'ecal_flat_logy'), fits, 'G4', norm=1, logy=1, logx=0)
+    #plot_ecal_flatten(x, os.path.join(dirname, 'ecal_flat_logx'), fits, 'G4', norm=1, logy=0, logx=1)
+    #plot_ecal_flatten(x, os.path.join(dirname, 'ecal_flat_logx_logy'), fits, 'G4', norm=1, logy=1, logx=1)
+    #plot_ecal_flatten(x, os.path.join(dirname, 'ecal_flat_logbins'), fits, 'G4', norm=1, logy=1, logx=1, logbin=1)
+    plot_ecal_flatten_fit(x, dirname, fits, "Cell", logy=0, norm=0, ifpdf=True, log=False)
     print('Result is saved in {}'.format(dirname))
     
 def GetData(datafile, numevents, scale=1, thresh=1e-6):
@@ -47,26 +47,25 @@ def GetData(datafile, numevents, scale=1, thresh=1e-6):
 def plot_ecal_flatten_fit(event, out_dir, fits, label, logy=0, norm=0, ifpdf=True, log=False):
     c1 = ROOT.TCanvas("c1" ,"" ,200 ,10 ,700 ,500) #make
     c1.SetGrid()
-    if not log:
-       ROOT.gPad.SetLogx()
+    #if not log:
+    ROOT.gPad.SetLogx()
     ROOT.gStyle.SetOptFit(1011)
     inv_pois = ROOT.TF1("inv_pois", "-1 *[0]*TMath::Poisson(x,[1])", -12, 3)
     pois = ROOT.TF1("pois", "[0]*TMath::Poisson(x,[1])", -12, 3)
-        
+    expon = ROOT.TF1("pois", "[0]*TMath::Power(x,[1]) + [3]")    
     title = "Cell energy deposits for 100-200 GeV"
     if logy:
         ROOT.gPad.SetLogy()
         title = title + " (logy) "
-    hd= ROOT.TH1F(label, "", 100, 1e-8, -1e+2)
-    if not log:
-        #r.BinLogX(hd)
-        print('log')
+    hd= ROOT.TH1F(label, "", 100, -8, -2)
+    r.BinLogX(hd)
+    
     data = event.flatten()
     print('The mean of data is {} and std is {}'.format(np.mean(data), np.std(data)))
-    indexes = np.where(data>0)
-    data = data[indexes]
+    #indexes = np.where(data>0)
+    #data = data[indexes]
     if log:
-       data= np.log(data[indexes])
+       data= np.log(data)
     print(np.amin(data), np.amax(data))
     r.fill_hist(hd, data)
     if norm:
@@ -106,10 +105,10 @@ def plot_ecal_flatten(event, out_file, fits, label, logy=0,  logx=0, norm=0, ifp
         title = title + " (logy) "
     data = event.flatten()
     if logbin: 
-      hd= ROOT.TH1F(label, "", 100, -10, 2)
+      hd= ROOT.TH1F(label, "", 100, -10, -2)
       r.BinLogX(hd)
     else:
-      hd= ROOT.TH1F(label, "", 100, 1e-10, 20)
+      hd= ROOT.TH1F(label, "", 100, 1e-10, 1e-2)
     indexes = np.where(data>0)
     data = data[indexes]
     print('The mean of data is {} and std is {}'.format(np.mean(data), np.std(data)))
