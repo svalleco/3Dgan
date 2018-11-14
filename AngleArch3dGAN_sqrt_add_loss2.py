83e451199df20279daeb61e459107e4d26e7dae5
gulrukhk <gulrukhk@yahoo.com>
2018-11-13 21:25:21 -0800

    Adding channel

--- AngleArch3dGAN_sqrt_add_loss2.py
+++ AngleArch3dGAN_sqrt_add_loss2.py
@@ -30,11 +30,11 @@ def safe_log(image):
 def count(image):
     #image =safe_log(image)
     bin1 = K.sum(K.tf.where(image > 0.1, K.ones_like(image), K.zeros_like(image)), axis=(1, 2, 3))
-    bin2 = K.sum(K.tf.where(K.tf.logical_and(image < 0.1, image > 0.01), K.ones_like(image), K.zeros_like(image)), axis=(1, 2, 3))
-    bin3 = K.sum(K.tf.where(K.tf.logical_and(image < 0.01, image > 0.001), K.ones_like(image), K.zeros_like(image)), axis=(1, 2, 3))
-    bin4 = K.sum(K.tf.where(K.tf.logical_and(image < 0.001, image > 0.0), K.ones_like(image), K.zeros_like(image)), axis=(1, 2, 3))
+    bin2 = K.sum(K.tf.where(K.tf.logical_and(image < 0.1, image > 0.05), K.ones_like(image), K.zeros_like(image)), axis=(1, 2, 3))
+    bin3 = K.sum(K.tf.where(K.tf.logical_and(image < 0.05, image > 0.01), K.ones_like(image), K.zeros_like(image)), axis=(1, 2, 3))
+    bin4 = K.sum(K.tf.where(K.tf.logical_and(image < 0.01, image > 0.0), K.ones_like(image), K.zeros_like(image)), axis=(1, 2, 3))
     bin5 = K.sum(K.tf.where(K.tf.equal(image, 0.0), K.ones_like(image), K.zeros_like(image)), axis=(1, 2, 3))
-    return K.concatenate([bin1, bin2, bin3, bin4, bin5], axis=1)
+    return K.expand_dims(K.concatenate([bin1, bin2, bin3, bin4, bin5], axis=1), -1)
 
 def ecal_angle(image):
     image = K.squeeze(image, axis=4)
