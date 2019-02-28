dedceb77203c2a3b54d29cc522db2f3923bdbacd
Gul Rukh Khattak <gkhattak@tlab-gpu-gtx1080ti-06.cern.ch>
2019-02-28 16:09:30 +0100

    removing unused

--- keras/EcalEnergyGan.py
+++ keras/EcalEnergyGan.py
@@ -6,8 +6,7 @@ import numpy as np
 
 import keras.backend as K
 from keras.layers import (Input, Dense, Reshape, Flatten, Lambda, merge,
-                          Dropout, Activation, Embedding)
-from BN2 import BatchNormalization 
+                          Dropout, BatchNormalization, Activation, Embedding)
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.convolutional import (UpSampling3D, Conv3D, ZeroPadding3D,
                                         AveragePooling3D)
@@ -15,7 +14,6 @@ from keras.layers.convolutional import (UpSampling3D, Conv3D, ZeroPadding3D,
 from keras.models import Model, Sequential
 
 def discriminator(keras_dformat='channels_last'):
-    K.set_image_data_format(keras_dformat)
     if keras_dformat =='channels_last':
         dshape=(25, 25, 25,1)
         daxis=(1,2,3)
@@ -23,44 +21,43 @@ def discriminator(keras_dformat='channels_last'):
         dshape=(1, 25, 25, 25)
         daxis=(2,3,4)
     image = Input(shape=dshape) 
-    x = Conv3D(32, (5, 5,5), padding='same')(image)
+    x = Conv3D(32, (5, 5,5), data_format=keras_dformat, padding='same')(image)
     x = LeakyReLU()(x)
     x = Dropout(0.2)(x)
 
     x = ZeroPadding3D((2, 2,2))(x)
-    x = Conv3D(8, (5, 5, 5), padding='valid')(x)
+    x = Conv3D(8, (5, 5, 5), data_format=keras_dformat, padding='valid')(x)
     x = LeakyReLU()(x)
     x = BatchNormalization()(x)
     x = Dropout(0.2)(x)
 
     x = ZeroPadding3D((2, 2, 2))(x)
-    x = Conv3D(8, (5, 5,5), padding='valid')(x)
+    x = Conv3D(8, (5, 5,5), data_format=keras_dformat, padding='valid')(x)
     x = LeakyReLU()(x)
     x = BatchNormalization()(x)
     x = Dropout(0.2)(x)
 
     x = ZeroPadding3D((1, 1, 1))(x)
-    x = Conv3D(8, (5, 5, 5), padding='valid')(x)
+    x = Conv3D(8, (5, 5, 5), data_format=keras_dformat, padding='valid')(x)
     x = LeakyReLU()(x)
     x = BatchNormalization()(x)
     x = Dropout(0.2)(x)
 
-    x = AveragePooling3D((2, 2, 2), data_format=keras_dformat)(x)
+    x = AveragePooling3D((2, 2, 2))(x)
     h = Flatten()(x)
 
-    dnn = Model(image, h)#.summary()
-    dnn.summary()
-    
+    dnn = Model(image, h)
+
     dnn_out = dnn(image)
 
     fake = Dense(1, activation='sigmoid', name='generation')(dnn_out)
     aux = Dense(1, activation='linear', name='auxiliary')(dnn_out)
     ecal = Lambda(lambda x: K.sum(x, axis=daxis))(image)
-    Model(input=[image], output=[fake, aux, ecal]).summary()
-    return Model(input=[image], output=[fake, aux, ecal])
+    Model(inputs=image, outputs=[fake, aux, ecal]).summary()
+    return Model(inputs=image, outputs=[fake, aux, ecal])
 
 def generator(latent_size=200, return_intermediate=False, keras_dformat='channels_last'):
-     K.set_image_data_format(keras_dformat)
+
      if keras_dformat =='channels_last':
         dim = (7,7,8,8)
      else:
@@ -69,37 +66,29 @@ def generator(latent_size=200, return_intermediate=False, keras_dformat='channel
      loc = Sequential([
          Dense(64 * 7* 7, input_dim=latent_size),
          Reshape(dim),
-         Conv3D(64, (6, 6, 8), padding='same', kernel_initializer='he_uniform'),
+         Conv3D(64, (6, 6, 8), data_format=keras_dformat, padding='same', kernel_initializer='he_uniform'),
          LeakyReLU(),
          BatchNormalization(),
          UpSampling3D(size=(2, 2, 2)),
 
          ZeroPadding3D((2, 2, 0)),
-         Conv3D(6, (6, 5, 8), kernel_initializer='he_uniform'),
+         Conv3D(6, (6, 5, 8), data_format=keras_dformat, kernel_initializer='he_uniform'),
          LeakyReLU(),
          BatchNormalization(),
          UpSampling3D(size=(2, 2, 3)),
 
          ZeroPadding3D((1,0,3)),
-         Conv3D(6, (3, 3, 8), kernel_initializer='he_uniform'),
+         Conv3D(6, (3, 3, 8), data_format=keras_dformat, kernel_initializer='he_uniform'),
          LeakyReLU(),
-         BatchNormalization(),
-
-         Conv3D(1, (2, 2, 2), use_bias=False, kernel_initializer='glorot_normal'),
+         Conv3D(1, (2, 2, 2), data_format=keras_dformat, use_bias=False, kernel_initializer='glorot_normal'),
          Activation('relu')
       
      ])
    
      latent = Input(shape=(latent_size, ))
-     loc.summary()
+     
      fake_image = loc(latent)
 
-     Model(input=[latent], output=[fake_image]).summary()
-     return Model(input=[latent], output=[fake_image])
+     Model(inputs=[latent], outputs=fake_image).summary()
+     return Model(inputs=[latent], outputs=fake_image)
 
-def main():
-    d = discriminator()
-    g = generator()
-
-if __name__ == '__main__':
-    main()
