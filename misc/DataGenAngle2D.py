a9071cb96a1a20bce82c6b5787ab153ad61a0d6a
gulrukhk <gulrukhk@yahoo.com>
2019-03-13 06:38:23 -0700

    scaling ecal

--- misc/DataGenAngle2D.py
+++ misc/DataGenAngle2D.py
@@ -9,19 +9,19 @@ import math
 import time
 import glob
 import numpy.core.umath_tests as umath
-import utils.GANutils as gan
-import utils.ROOTutils as r
-import utils.RootPlotsGAN as pl
 import setGPU #if Caltech
 
-sys.path.insert(0,'/nfshome/gkhattak/3Dgan')
+sys.path.insert(0,'../')
+import analysis.utils.GANutils as gan
+import analysis.utils.ROOTutils as r
+import analysis.utils.RootPlotsGAN as pl
 
 def main():
    datapath = "/data/shared/gkhattak/*Measured3ThetaEscan/*.h5" # path to data
    # path to generator weights
-   genweight = "/nfshome/gkhattak/3Dgan/weights/3Dweights_1loss_50weight_sqrt/params_generator_epoch_040.hdf5"
+   genweight = "/nfshome/gkhattak/3Dgan/weights/3dgan_weights_bins_pow_p85/params_generator_epoch_059.hdf5"
    # generator model
-   from AngleArch3dGAN_sqrt import generator
+   from AngleArch3dGAN import generator
 
    latent = 256 # latent space for generator
    g=generator(latent) # build generator
@@ -37,32 +37,34 @@ def main():
    thetas = [62, 90, 118] # angle bins
    ang = 1 # use all calculation for variable angle
    xscale = 1 # scaling of images
+   xpower = 0.85
    ascale=1
-   post = square # post processing: It can be either scale (without sqrt) or square(sqrt)
+   ecalscale=50. # scaling in original data set
+   post = inv_power # post processing: It can be either scale (without sqrt) or square(sqrt)
    thresh = 0 # if using threshold
-   plotsdir = 'results/new_genplots_compare_ep40' # name of folder to save results
+   plotsdir = 'results/new_genplots_compare_bins_powp85_scaled' # name of folder to save results
    gan.safe_mkdir(plotsdir) # make plot directory
    opt="colz" # option for 2D hist
    angtype='theta'
-   datafiles = gan.GetDataFiles(datapath, numdata, Particles=['Ele']) # get list of files
-   var = gan.get_sorted_angle(datafiles, energies, True, num_events1, num_events2, angtype=angtype, thresh=thresh) # returning a dict with sorted data.
+   datafiles = gan.GetDataFiles(datapath, Particles=['Ele']) # get list of files
+   var = gan.get_sorted_angle(datafiles[25:], energies, True, num_events1, num_events2, angtype=angtype, thresh=thresh) # returning a dict with sorted data.
    for energy in energies:
       edir = os.path.join(plotsdir, 'energy{}'.format(energy))
       gan.safe_mkdir(edir)
       rad = np.radians(thetas)
       for index, a in enumerate(rad):
-         adir = os.path.join(edir, 'angle{}'.format(a))
+         adir = os.path.join(edir, 'angle{}'.format(thetas[index]))
          gan.safe_mkdir(adir)
          indexes = np.where(((var["angle" + str(energy)]) > a - tolerance2) & ((var["angle" + str(energy)]) < a + tolerance2)) # all events with angle within a bin
          # angle bins are added to dict
-         var["events_act" + str(energy) + "ang_" + str(index)] = var["events_act" + str(energy)][indexes]
+         var["events_act" + str(energy) + "ang_" + str(index)] = var["events_act" + str(energy)][indexes]/ecalscale
          var["energy" + str(energy) + "ang_" + str(index)] = var["energy" + str(energy)][indexes]
          var["angle" + str(energy) + "ang_" + str(index)] = var["angle" + str(energy)][indexes]
          var["index" + str(energy)+ "ang_" + str(index)] = var["events_act" + str(energy) + "ang_" + str(index)].shape[0]
          var["events_gan" + str(energy) + "ang_" + str(index)]= gan.generate(g, var["index" + str(energy)+ "ang_" + str(index)],
                                                                            [var["energy" + str(energy)+ "ang_" + str(index)]/100,
                                                                             (var["angle"+ str(energy)+ "ang_" + str(index)]) * ascale], latent)
-         var["events_gan" + str(energy) + "ang_" + str(index)]= post(var["events_gan" + str(energy) + "ang_" + str(index)], xscale)
+         var["events_gan" + str(energy) + "ang_" + str(index)]= post(var["events_gan" + str(energy) + "ang_" + str(index)], xpower=xpower, xscale=ecalscale)
          ddir = os.path.join(adir, 'G4Data')
          gan.safe_mkdir(ddir)
          gdir =os.path.join(adir, 'GAN')
@@ -88,11 +90,11 @@ def main():
             
    print('Plots are saved in {}'.format(plotsdir))
     
-def square(n, xscale):
-   return np.square(n)/xscale
+def power(n, xscale=1, xpower=1):
+   return np.power(n/xscale, xpower)
 
-def scale(n, xscale):
-   return n / xscale
+def inv_power(n, xscale=1, xpower=1):
+   return np.power(n, 1./xpower) / xscale
 
 def applythresh(n, thresh):
    n[n<thresh]=0
