750ba43626119235bfcc57d911150fe6f5a306b3
gulrukhk <gulrukhk@yahoo.com>
2019-07-19 07:49:55 -0700

    adding profile

--- misc/FlatHistData_bins.py
+++ misc/FlatHistData_bins.py
@@ -18,13 +18,14 @@ import setGPU
 
 def main():
    datapath = "/bigdata/shared/gkhattak/EleMeasured3ThetaEscan/Ele_VarAngleMeas_100_200_000.h5"
-   outdir = 'results/data_bins_spectrum'
+   outdir = 'results/data_bins_spectrum_tprof'
    gan.safe_mkdir(outdir)
    outfile = os.path.join(outdir, 'Ecal')
    x, y, ang=GetAngleData(datapath) #, ftn=np.sqrt)
    print('The angle data varies from {} to {}'.format(np.amin(x[x>0]), np.amax(x)))
    labels = ['G4 Var. Angle']
-   bins=[0.05, 0,03, 0.02, 0.0125, 0.008, 0.003]
+   
+   bins=[0.05, 0.03, 0.02, 0.0125, 0.008, 0.003]
    scale = 50.
    bins.extend([0, -0.003])
    bins = sorted(bins)
@@ -33,12 +34,12 @@ def main():
    edges = np.zeros(len(bins))
    for i, bin in enumerate(bins):
      edges[i] = bins[i]/scale
-   print (edges[3:])
    p=[int(np.amin(y)), int(np.amax(y))]
    plot_ecal_hist_bins([x/scale], edges, outfile, y, labels, logy=0, logx=0, norm=2, p=p, ifpdf=True)   
    plot_ecal_hist_bins([x/scale], edges, outfile+'_ylog', y, labels, logy=1, logx=0, norm=2, p=p, ifpdf=True)
    plot_ecal_hist_bins([x/scale], edges[3:], outfile+'_xylog', y, labels, logy=1, logx=1, norm=2, p=p, ifpdf=True)
    plot_ecal_hist_bins([x/scale], edges[3:], outfile+'_xlog', y, labels, logy=0, logx=1, norm=2, p=p, ifpdf=True)
+   plot_ecal_prof_bins([x], outfile+'_prof', y, labels, scale=scale, logy=0, logx=0, norm=2, p=p, ifpdf=True)
    print('Histogram is saved in ', outfile)
          
 def GetAngleData(datafile, numevents=1000, ftn=0, scale=1, angtype='theta'):
@@ -51,6 +52,20 @@ def GetAngleData(datafile, numevents=1000, ftn=0, scale=1, angtype='theta'):
       x = ftn(x)
    ang = np.array(f.get(angtype)[:numevents])
    return x, y, ang
+
+def hist_count(x, p=1.0, daxis=(0, 1, 2), limits=[0.05, 0.03, 0.02, 0.0125, 0.008, 0.003]):
+    limits=np.array(limits) # bin boundaries used
+    limits= np.power(limits, p)
+    bin1 = np.sum(np.where(x>(limits[0]) , 1, 0), axis=daxis)
+    bin2 = np.sum(np.where((x<(limits[0])) & (x>(limits[1])), 1, 0), axis=daxis)
+    bin3 = np.sum(np.where((x<(limits[1])) & (x>(limits[2])), 1, 0), axis=daxis)
+    bin4 = np.sum(np.where((x<(limits[2])) & (x>(limits[3])), 1, 0), axis=daxis)
+    bin5 = np.sum(np.where((x<(limits[3])) & (x>(limits[4])), 1, 0), axis=daxis)
+    bin6 = np.sum(np.where((x<(limits[4])) & (x>(limits[5])), 1, 0), axis=daxis)
+    bin7 = np.sum(np.where((x<(limits[5])) & (x>0.), 1, 0), axis=daxis)
+    bin8 = np.sum(np.where(x==0, 1, 0), axis=daxis)
+    bins = [bin1, bin2, bin3, bin4, bin5, bin6, bin7, bin8]
+    return bins
 
 def plot_ecal_flatten_hist_bins(events, bins, out_file, energy, labels, logy=0, norm=0, ifpdf=True, p=[2, 500]):
    c1 = ROOT.TCanvas("c1" ,"" ,200 ,10 ,700 ,500) #make
@@ -113,11 +128,11 @@ def plot_ecal_hist_bins(events, bin_edges, out_file, energy, labels, logy=0, log
    for i, (event, label) in enumerate(zip(events, labels)):
       hds.append(ROOT.TH1F(label, "", len(bin_edges)-1, bin_edges))
       hd = hds[i]
-      hd.SetStats(0)
-      #if logx:
-      #  r.BinLogX(hd)
-      data = event.flatten()
-      r.fill_hist(hd, data)
+      #hd.SetStats(0)
+      #hd.Sumw2()
+      for e in event:
+        data = e.flatten()
+        r.fill_hist(hd, data)
       if norm:
         r.normalize(hd, norm-1)
       hd.SetLineColor(color)
@@ -143,6 +158,52 @@ def plot_ecal_hist_bins(events, bin_edges, out_file, energy, labels, logy=0, log
    else:
      c1.Print(out_file + '.C')
 
+def plot_ecal_prof_bins(events, out_file, energy, labels, scale=1, bins = [0.05, 0.03, 0.02, 0.0125, 0.008, 0.003], logy=0, logx=0, norm=0, ifpdf=True, p=[2, 500]):
+   c1 = ROOT.TCanvas("c1" ,"" ,200 ,10 ,700 ,500) #make
+   xticks = sorted([10] + bins + [0])
+   xticks = [_/scale for _ in xticks]
+   print(xticks)                                                                                                                                                              
+   c1.SetGrid()
+   if logx:
+     ROOT.gPad.SetLogx()
+   title = "Cell energy {:d}-{:d} GeV Primary".format(p[0], p[1])
+   legend = ROOT.TLegend(.4, .8, .6, .9)
+   color =2
+   if logy:
+      ROOT.gPad.SetLogy()
+   hds=[]
+   for i, (event, label) in enumerate(zip(events, labels)):
+      hds.append(ROOT.TProfile(label, "", len(bins) + 1, 0, len(bins)+ 1, "S"))
+      hd = hds[i]
+      hd.SetStats(0)
+      for e in event:
+         counts = hist_count(e, limits=bins)
+         for c in np.arange(len(counts)-1):
+           hd.Fill(c, counts[c])
+      hd.SetLineColor(color)
+      if i ==0:
+        hd.SetTitle(title)
+        hd.GetXaxis().SetTitle("Single cell depositions Bins [GeV]")
+        for c in np.arange(1, len(counts)+ 1):
+          hd.GetXaxis().ChangeLabel(c, -1, -1, -1, -1, -1, str(xticks[c-1]))
+        hd.GetYaxis().SetTitle("Counts")
+        hd.GetYaxis().CenterTitle()
+        hd.Draw()
+        #hd.Draw('sames hist')                                                                                                                                                                                        
+      else:
+        hd.Draw('sames')
+        #hd.Draw('sames hist')                                                                                                                                                                                        
+      color+=2
+      legend.AddEntry(hd,label ,"l")
+      c1.Modified()
+      c1.Update()
+
+   legend.Draw()
+   c1.Update()
+   if ifpdf:
+     c1.Print(out_file + '.pdf')
+   else:
+     c1.Print(out_file + '.C')
 
 if __name__ == "__main__":
    main()
