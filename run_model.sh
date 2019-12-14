579405325c05d1e5a20fa44d409409d63abe281d
Sofia Vallecorsa <svalleco@lxplus770.cern.ch>
2019-12-14 09:03:29 +0100

    Load all data at once

--- run_model.sh
+++ run_model.sh
@@ -3,4 +3,5 @@ mkdir Data
 mkdir Results
 aws s3 cp --no-sign-request --endpoint-url=https://s3.cern.ch s3://gan-bucket/ Data/. --recursive --exclude "EleEscan_EleEscan*"
 cd keras
-python3 --datapath $PWD/Data --name='tpu_training' --nbepochs 1 --tpu=$TPU_NAME AngleTrain3dGAN_tf.py
+#python3 --datapath $PWD/Data --name='tpu_training' --nbepochs 1 --tpu=$TPU_NAME AngleTrain3dGAN_tf.py
+python3 AngleTrain3dGAN_notpu.py --datapath $PWD/Data  --nbepochs 1
