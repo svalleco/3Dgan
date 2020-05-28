9b2e07b2fac6a15807fd3ec036f6b2c5bc006269
Gulrukh Khattak <gkhattak@imperium-sm.hep.caltech.edu>
2020-05-28 03:21:37 -0700

    custom layer with samplewise batch normalization

--- keras/archives/BN2.py
+++ keras/archives/BN2.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""custom Normalization layers.
+"""custom Normalization layers with sample wise normalization.
 """
 from __future__ import absolute_import
 from __future__ import division
