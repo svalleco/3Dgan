5f3c485dabda1896bd6f0cf6c4c4c7f4f54f0597
Gulrukh Khattak <gkhattak@imperium-sm.hep.caltech.edu>
2020-06-06 07:03:18 -0700

    sampling fraction for theta bins

--- keras/misc/Ecalsum.py
+++ keras/misc/Ecalsum.py
@@ -9,47 +9,78 @@ import time
 import glob
 import sys
 import numpy.core.umath_tests as umath
-sys.path.insert(0,'/nfshome/gkhattak/3Dgan/analysis')
-sys.path.insert(0,'/nfshome/gkhattak/3Dgan')
-sys.path.insert(0,'/nfshome/gkhattak/keras/architectures_tested/')
+sys.path.insert(0,'../keras/analysis')
+sys.path.insert(0,'../')
+#sys.path.insert(0,'/nfshome/gkhattak/keras/architectures_tested/')
 import utils.GANutils as gan
 import utils.ROOTutils as r
 import setGPU
 
 def main():
-   datapath = "/data/shared/LCDLargeWindow/varangle/EleEscan/EleEscan_RandomAngle_2_1.h5"
-   datapath2 = "/data/shared/LCDLargeWindow/fixedangle/EleEscan/EleEscan_1_1.h5"
-   datapath3 = '/bigdata/shared/LCD/NewV1/EleEscan/EleEscan_1_1.h5'
-
+   datapath = "/storage/group/gpu/bigdata/LCDLargeWindow/LCDLargeWindow/varangle/EleEscan/EleEscan_RandomAngle_*.h5"
+   datapath2 = '../../Ele_generated_0.hdf5'
+   datafiles =  sorted( glob.glob(datapath))
    numdata = 5000
    scale=1
-   outdir = 'results/data_compare_ecal'
+   dscale = 50.
+   angle = [62, 118]
+   outdir = 'results/data_compare_ecal_angles'
    gan.safe_mkdir(outdir)
    outfile = os.path.join(outdir, 'Ecal')
-   x, y, ang=GetAngleData(datapath, numdata) #, ftn=np.sqrt)
-   print('The angle data varies from {} to {}'.format(np.amin(x[x>0]), np.amax(x)))
-   x2, y2=GetData(datapath2, numdata, thresh=0)
-   print('The fixed angle new data varies from {} to {}'.format(np.amin(x2[x2>0]), np.amax(x2)))
-   x3, y3=GetData2(datapath3, numdata, thresh=0)
-   print('The fixed angle old  varies from {} to {}'.format(np.amin(x3[x3>0]), np.amax(x3)))
-   min_p = np.amin([np.amin(y), np.amin(y2), np.amin(y3)])
-   max_p = np.amax([np.amax(y), np.amax(y2), np.amax(y3)])
+   for i in range(10):
+     if i ==0:
+       x, y, ang=GetAngleData(datafiles[i], numdata) #, ftn=np.sqrt)
+       indexes = np.where((ang>(np.radians(angle[0])-0.1)) & ((ang<(np.radians(angle[0])+0.1))))
+       x1 = x[indexes]
+       y1 = y[indexes]
+       ang1 = ang[indexes]
+   
+       x1 = x1/dscale
+       indexes = np.where((ang>(np.radians(angle[1])-0.1)) & ((ang<(np.radians(angle[1])+0.1))))
+       x2 = x[indexes]
+       y2 = y[indexes]
+       ang2 = ang[indexes]
+   
+       x2 = x2/dscale 
+
+     else:
+       x, y, ang=GetAngleData(datafiles[i], numdata)
+       indexes = np.where((ang>(np.radians(angle[0])-0.1)) & ((ang<(np.radians(angle[0])+0.1))))
+       x1 = np.concatenate((x1, x[indexes]), axis=0)
+       y1 = np.concatenate((y1, y[indexes]), axis=0)
+       ang1 = np.concatenate((ang1, ang[indexes]), axis=0)
+
+       indexes = np.where((ang>(np.radians(angle[1])-0.1)) & ((ang<(np.radians(angle[1])+0.1))))
+       x2 = np.concatenate((x2, x[indexes]), axis=0)
+       y2 = np.concatenate((y2, y[indexes]), axis=0)
+       ang2 = np.concatenate((ang2, ang[indexes]), axis=0)
+   x1 = x1/dscale
+   x2 = x2/dscale
+   print(x1.shape[0])
+   print(x2.shape[0])
+   min_p = np.amin([np.amin(y), np.amin(y2)])
+   max_p = np.amax([np.amax(y), np.amax(y2)])
    p = [int(min_p), int(max_p)]
    print(p) 
 
-   labels = ['G4 Var. Angle', 'G4 Fixed Angle(new)', 'G4 Fixed Angle(old)']
+   labels = ['G4 ' + str(angle[0]), 'G4 ' + str(angle[1])]
    
-   ecal1 = np.sum(x, axis=(1, 2, 3))
+   ecal1 = np.sum(x1, axis=(1, 2, 3))
    ecal2 = np.sum(x2, axis=(1, 2, 3))
-   ecal3 = np.sum(x3, axis=(1, 2, 3))
-   sf1= np.sum(x, axis=(1, 2, 3))/(y *50.)
-   sf2= np.sum(x2, axis=(1, 2, 3))/(y2 * 50.)
-   sf3= np.sum(x3, axis=(1, 2, 3))/y3
-   plot_ecal_hist([ecal1/50., ecal2/50., ecal3], outfile, y, labels, logy=0, norm=2, p=p)
-   plot_ecal_hist([ecal1/50., ecal2/50., ecal3], outfile + '_log', y, labels, logy=1, norm=2, p=p)
-   print('Histogram is saved in ', outfile)
-   print('Histogram is saved in ', outfile+ '_log')
-       
+   
+   print('The G4 data sum varies from {} to {}'.format(np.amin(ecal1), np.amax(ecal2)))
+   print('The generated data sum varies from {} to {}'.format(np.amin(ecal2), np.amax(ecal2)))
+   sf1= ecal1/y1
+   sf2= ecal2/y2
+   
+   
+   plot_ecal_prof([sf1, sf2], [y1, y2], outfile+'_prof', y, labels, logy=0, norm=0, p=p)
+   plot_ecal_prof([ecal1, ecal2], [y1, y2], outfile + 'prof_log', y, labels, logy=1, norm=0, p=p)
+   plot_ecal_hist([ecal1, ecal2], outfile+'_', labels, logy=0, norm=0, p=p)
+   plot_ecal_hist([ecal1, ecal2], outfile + '_log',  labels, logy=1, norm=0, p=p)
+
+   print('Histogram are saved in ', outdir)
+          
 def postproc(event, f, scale):
    return f(event)/scale
 
@@ -85,7 +116,7 @@ def GetData2(datafile, numevents, scale=1, thresh=1e-6):
    return x, y
                      
 
-def plot_ecal_hist(events, out_file, energy, labels, logy=0, norm=0, ifpdf=True, p=[2, 500]):
+def plot_ecal_hist(events, out_file, labels, logy=0, norm=0, ifpdf=True, p=[2, 500]):
    c1 = ROOT.TCanvas("c1" ,"" ,200 ,10 ,700 ,500) #make
    c1.SetGrid()
    #ROOT.gPad.SetLogx()
@@ -98,7 +129,7 @@ def plot_ecal_hist(events, out_file, energy, labels, logy=0, norm=0, ifpdf=True,
       title = title + " (log)"
    hds=[]
    for i, (event, label) in enumerate(zip(events, labels)):
-      hds.append(ROOT.TH1F(label, "", 50, 0., p[1]*1.1/50.))
+      hds.append(ROOT.TH1F(label, "", 100, 0., 12))
       hd = hds[i]
       hd.SetStats(0)
       #r.BinLogX(hd)
@@ -144,7 +175,7 @@ def plot_ecal_prof(events, energies, out_file, energy, labels, logy=0, norm=0, i
    for i, (event, e, label) in enumerate(zip(events, energies, labels)):
       hds.append(ROOT.TProfile(label, "", 100, p[0], p[1]*1.1))
       hd = hds[i]
-      hd.SetStats(0)
+      #hd.SetStats(0)
       #r.BinLogX(hd)
       data = event
       r.fill_profile(hd, data, e)
@@ -153,8 +184,8 @@ def plot_ecal_prof(events, energies, out_file, energy, labels, logy=0, norm=0, i
       hd.SetLineColor(color)
       if i ==0:
         hd.SetTitle(title)
-        hd.GetXaxis().SetTitle("Ep GeV")
-        hd.GetYaxis().SetTitle("Count")
+        hd.GetXaxis().SetTitle("E_p [GeV]")
+        hd.GetYaxis().SetTitle("S_F")
         hd.GetYaxis().CenterTitle()
         hd.Draw()
         hd.Draw('sames hist')
