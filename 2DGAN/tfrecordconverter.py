44973f5a75a0358a43366c2b37b8645259fc2138
recardoso <renatocardoso1996@hotmail.com>
2021-01-11 12:14:44 +0100

    Generate dataset files

--- 2DGAN/tfrecordconverter.py
+++ 2DGAN/tfrecordconverter.py
@@ -8,7 +8,7 @@ import h5py
 
 import glob
 
-import analysis.utils.GANutils as gan
+#import analysis.utils.GANutils as gan
 
 
 def get_parser():
@@ -54,11 +54,19 @@ def retrieve_ECAL(ecalarray, xscale=1, originalarraydim=[5000,25, 25, 25]): #mis
     #sizes: 5000, 25, 25, 25
     return tf.reshape(ecalarray, originalarraydim)
 
-def data_preperation(X, y, keras_dformat, batch_size, percent=100):      #data preperation
+def data_preperation(f, keras_dformat, percent=100):      #data preperation
+
+    X = np.array(f.get('ECAL'))
+    e_file = f.get('target')
+    y = np.array(e_file[:,1])
+    
 
     # Missing Loading data
 
     X[X < 1e-6] = 0  #remove unphysical values
+
+    X_train = X
+    y_train = y
 
     # X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.9, test_size=0.1)
 
@@ -89,10 +97,10 @@ def data_preperation(X, y, keras_dformat, batch_size, percent=100):      #data p
     #####################################################################################
     nb_train = X_train.shape[0]
     #nb_test =  X_test.shape[0]
-    if nb_train < batch_size:
-        print("\nERROR: batch_size is larger than trainings data")
-        print("batch_size: ", batch_size)
-        print("trainings data: ", nb_train, "\n")
+    #if nb_train < batch_size:
+    #    print("\nERROR: batch_size is larger than trainings data")
+    #    print("batch_size: ", batch_size)
+    #    print("trainings data: ", nb_train, "\n")
 
     X_train = X_train.astype(np.float32)  #alle Werte in Floats umwandeln
     #X_test = X_test.astype(np.float32)
@@ -104,21 +112,35 @@ def data_preperation(X, y, keras_dformat, batch_size, percent=100):      #data p
     else:
         ecal_train = np.sum(X_train, axis=(2, 3, 4))
         #ecal_test = np.sum(X_test, axis=(2, 3, 4))
+    ecal_train = ecal_train.flatten()
     return {'X': X_train,'Y': y_train, 'ecal': ecal_train}
     #return X_train, X_test, y_train, y_test, ecal_train, ecal_test, nb_train, nb_test
 
 #convert dataset with preprocessing
-def ConvertH5toTFRecordPreprocessing(datafile,filenumber,datadirectory):
+def ConvertH5toTFRecordPreprocessing(datafile,filenumber,series,datadirectory):
     # read file
     print('Loading Data from .....', datafile)
     f=h5py.File(datafile,'r')
     
     #dataset = GetDataAngleParallel(f)
-    dataset = data_preperation()
+    dataset = data_preperation(f, 'channels_first')
+
+    f.close()
+
+    print(len(dataset.get('X')))
+    print(len(dataset.get('Y')))
+    print(len(dataset.get('ecal')))
+    #return
 
     dataset = tf.data.Dataset.from_tensor_slices((dataset.get('X'),dataset.get('Y'),dataset.get('ecal')))#.batch(128)
 
-    tf.print(dataset)
+    ##tf.print(tf.shape(dataset))
+    #for f0, f1, f2 in dataset.take(10):
+    #    print(f0)
+    #    print(f1)
+    #    print(f2)
+
+    #return
 
     print('Start')
     def serialize(feature1,feature2,feature3):
@@ -133,7 +155,7 @@ def ConvertH5toTFRecordPreprocessing(datafile,filenumber,datadirectory):
         )
         return finaldata.SerializeToString()
 
-    def serialize_example(f0,f1,f2,f3):
+    def serialize_example(f0,f1,f2):
         tf_string = tf.py_function(serialize,(f0,f1,f2),tf.string)
         return tf.reshape(tf_string, ())
 
@@ -143,7 +165,7 @@ def ConvertH5toTFRecordPreprocessing(datafile,filenumber,datadirectory):
         
 
     #Rewrite file names
-    filename = datadirectory + '/Ele_VarAngleMeas_100_200_{0:03}.tfrecords'.format(filenumber)
+    filename = datadirectory + '/EleEscan_'+ str(series) +'_'+ str(filenumber) +'.tfrecords'
     print('Writing data in .....', filename)
     writer = tf.data.experimental.TFRecordWriter(str(filename))
     writer.write(serialized_dataset)
@@ -152,6 +174,11 @@ def ConvertH5toTFRecordPreprocessing(datafile,filenumber,datadirectory):
 
 def RetrieveTFRecordpreprocessing(recorddatapaths, batch_size):
     recorddata = tf.data.TFRecordDataset(recorddatapaths, num_parallel_reads=tf.data.experimental.AUTOTUNE)
+    
+    #ds_size = sum(1 for _ in recorddata)
+
+    #print(ds_size)
+    #return
 
     options = tf.data.Options()
     options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
@@ -171,14 +198,14 @@ def RetrieveTFRecordpreprocessing(recorddatapaths, batch_size):
         # Parse the input `tf.Example` proto using the dictionary above.
         data = tf.io.parse_single_example(example_proto, retrieveddata)
         data['X'] = tf.reshape(data['X'],[1, 25, 25, 25]) #need to get size [1, 25, 25, 25] for channel first [25, 25, 25, 1] for channel last
-        #print(tf.shape(data['Y']))
+        #tf.print(tf.shape(data['Y']))
         data['Y'] = tf.reshape(data['Y'],[1])
-        data['ang'] = tf.reshape(data['ang'],[1])
+        #:data['ang'] = tf.reshape(data['ang'],[1])
         data['ecal'] = tf.reshape(data['ecal'],[1])
-        #print(tf.shape(data['Y']))
+        print(data['Y'])
         return data
 
-    parsed_dataset = recorddata.map(_parse_function, num_parallel_calls=tf.data.experimental.AUTOTUNE).cache().shuffle(124987).repeat().batch(batch_size, drop_remainder=True).with_options(options)
+    parsed_dataset = recorddata.map(_parse_function, num_parallel_calls=tf.data.experimental.AUTOTUNE).cache().shuffle(200000).repeat().batch(batch_size, drop_remainder=True).with_options(options)
 
     return parsed_dataset
 
@@ -189,13 +216,34 @@ if __name__ == '__main__':
     datapath = params.datapath# Data path
     outpath = params.outpath # training output
     Files = sorted( glob.glob(datapath))
+    file10 = Files[1]
+    Files.remove(file10)
+    Files.insert(9, file10)
+    file10 = Files[11]
+    Files.remove(file10)
+    Files.insert(19, file10)
     print ("Found {} files. ".format(len(Files)))
+    print(Files)
 
-    filenumber = 0
+    #feat = RetrieveTFRecordpreprocessing('/eos/user/r/redacost/EleScanstf/EleEscan_1_1.tfrecords',100)
+    #bat = 0
+    #for b in feat:
+    #    bat += 1
+    #    print(b.get('Y'))
+    #    print(bat)
+    #    #print(feat.get('Y'))
+
+    #    exit()
+    
+    filenumber = 1
+    series = 1
 
     for f in Files:
+        if filenumber == 11:
+            filenumber = 1
+            series = 2
         print(filenumber)
-        finaldata = ConvertH5toTFRecordPreprocessing(f,filenumber,outpath)
+        finaldata = ConvertH5toTFRecordPreprocessing(f,filenumber,series,outpath)
         #print(finaldata)
         filenumber += 1
         #feat = RetrieveTFRecordpreprocessing(outpath)
